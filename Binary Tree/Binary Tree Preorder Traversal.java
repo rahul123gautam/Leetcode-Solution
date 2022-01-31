4aea104ff6e3c07fd1ef9175f0b4143193f7758c
Rahul Maurya <93531189+rahul123gautam@users.noreply.github.com>
2022-01-31 18:04:43 +0530

    Update Binary Tree Preorder Traversal.java

--- Binary Tree/Binary Tree Preorder Traversal.java
+++ Binary Tree/Binary Tree Preorder Traversal.java
@@ -3,7 +3,7 @@ Problem Link : https://leetcode.com/problems/binary-tree-preorder-traversal/
 
                                   ***************CODE****************
                                   
-                                    
+ **************************USING RECURSION****************************                                   
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -33,4 +33,44 @@ class Solution {
         preorder(root.left , ans);
         preorder(root.right , ans);
     }
+  
+  
 }
+  ********************ITRATIVE METHOD*********************
+  
+    
+    
+    
+    class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer>ans = new ArrayList<>();
+    //     preorder(root , ans);
+    //     return ans;
+    // }
+    // public void preorder(TreeNode root , List<Integer>ans){
+    //     if(root == null){
+    //         return;
+    //     }
+    //     ans.add(root.val);
+    //     preorder(root.left , ans);
+    //     preorder(root.right , ans);
+        
+        Stack<TreeNode>st = new Stack<>();
+        if(root == null){
+            return ans;
+        }
+        st.add(root);
+        while(!st.isEmpty()){
+            root = st.pop();
+            ans.add(root.val);
+            if(root.right != null){
+                st.push(root.right);
+            }
+            if(root.left != null){
+                st.push(root.left);
+            }
+        }
+        return ans;
+    }
+}
+
