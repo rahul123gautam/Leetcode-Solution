4d2001fdbb77b8f2ca3aca71a71d496c2d9f3289
Rahul Maurya <93531189+rahul123gautam@users.noreply.github.com>
2022-02-22 11:13:49 +0530

    Create  Minimum Number of Steps to Make Two Strings Anagram.java

---
+++ String/Minimum Number of Steps to Make Two Strings Anagram.java
@@ -0,0 +1,68 @@
+Problem link :- https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/
+
+
+You are given two strings of the same length s and t. In one step you can choose any character of t and replace it with another character.
+
+Return the minimum number of steps to make t an anagram of s.
+
+An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+
+ 
+
+Example 1:
+
+Input: s = "bab", t = "aba"
+Output: 1
+Explanation: Replace the first 'a' in t with b, t = "bba" which is anagram of s.
+Example 2:
+
+Input: s = "leetcode", t = "practice"
+Output: 5
+Explanation: Replace 'p', 'r', 'a', 'i' and 'c' from t with proper characters to make t anagram of s.
+Example 3:
+
+Input: s = "anagram", t = "mangaar"
+Output: 0
+Explanation: "anagram" and "mangaar" are anagrams. 
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+s.length == t.length
+s and t consist of lowercase English letters only.
+  
+  
+                        ********************CODE*********************
+                        
+  
+  
+  class Solution {
+    public int minSteps(String s, String t) {
+       HashMap<Character , Integer>map1 = new HashMap<>();
+       HashMap<Character , Integer>map2 = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            if(map1.containsKey(s.charAt(i))){
+                map1.put(s.charAt(i) , map1.getOrDefault(s.charAt(i) , 0) + 1);
+            }else{
+                map1.put(s.charAt(i) , 1);
+            }
+            if(map2.containsKey(t.charAt(i))){
+                map2.put(t.charAt(i) , map2.getOrDefault(t.charAt(i) , 0) + 1);
+            }else{
+                map2.put(t.charAt(i) , 1);
+            }
+        }
+        int count = 0;
+        for(char ch : map1.keySet()){
+            if(map2.containsKey(ch)){
+                if(map1.get(ch) > map2.get(ch)){
+                   count += map1.get(ch) - map2.get(ch);
+                }
+            }else{
+                count += map1.get(ch);
+            }
+        }
+        return count;
+    }
+}
