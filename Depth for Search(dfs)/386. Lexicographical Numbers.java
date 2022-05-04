ddccdce7e751273a0395ea5d557d448bf11ba9ea
Rahul Maurya <93531189+rahul123gautam@users.noreply.github.com>
2022-05-04 18:16:08 +0530

    Create 386. Lexicographical Numbers.java

---
+++ Depth for Search(dfs)/386. Lexicographical Numbers.java
@@ -0,0 +1,42 @@
+Problem Link :- Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+
+You must write an algorithm that runs in O(n) time and uses O(1) extra space. 
+
+ 
+
+Example 1:
+
+Input: n = 13
+Output: [1,10,11,12,13,2,3,4,5,6,7,8,9]
+Example 2:
+
+Input: n = 2
+Output: [1,2]
+ 
+
+Constraints:
+
+1 <= n <= 5 * 104
+  
+  
+  
+                           *************************CODE***************************
+                           
+  
+  
+  class Solution {
+    List<Integer>ans = new ArrayList<>();
+    public List<Integer> lexicalOrder(int n) {
+        for(int i = 1; i <= 9; i++){
+            dfs(i , n);
+        }
+        return ans;
+    }
+    public void dfs(int i , int n){
+        if(i > n) return;
+        ans.add(i);
+        for(int j = 0; j <= 9; j++){
+            dfs(10*i + j , n);
+        }
+    }
+}
