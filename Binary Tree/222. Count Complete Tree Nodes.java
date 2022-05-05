67fa2785e6c60d177127003054339b71a7d15d99
Rahul Maurya <93531189+rahul123gautam@users.noreply.github.com>
2022-05-05 18:32:28 +0530

    Create 222. Count Complete Tree Nodes.java

---
+++ Binary Tree/222. Count Complete Tree Nodes.java
@@ -0,0 +1,56 @@
+Problem Link :- https://leetcode.com/problems/count-complete-tree-nodes/
+
+
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Design an algorithm that runs in less than O(n) time complexity.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5,6]
+Output: 6
+Example 2:
+
+Input: root = []
+Output: 0
+Example 3:
+
+Input: root = [1]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5 * 104].
+0 <= Node.val <= 5 * 104
+The tree is guaranteed to be complete.    
+  
+  
+  
+  
+                                   *****************************CODE*************************************
+                                   
+  
+  class Solution {
+    public int countNodes(TreeNode root) {
+        int ans = 0;
+        Queue<TreeNode>q = new LinkedList<>();
+        if(root == null) return ans;
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode temp = q.poll();
+                ans++;
+                if(temp.left != null) q.add(temp.left);
+                if(temp.right != null) q.add(temp.right);
+            }
+        }
+        return ans;
+    }
+}
