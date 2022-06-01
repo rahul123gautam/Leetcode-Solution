b73733cd5155c0ae945169ff488132f4076df8a4
Rahul Maurya <93531189+rahul123gautam@users.noreply.github.com>
2022-06-01 22:18:01 +0530

    Create 55. Jump Game.java

---
+++ Dynamic Progarammin/55. Jump Game.java
@@ -0,0 +1,15 @@
+Problem Link:- https://leetcode.com/problems/jump-game/
+
+
+ ************************CODE*************************
+ 
+   class Solution {
+    public boolean canJump(int[] nums) {
+       int reachable = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(reachable < i) return false;
+            reachable = Math.max(reachable , nums[i] + i);
+        }
+        return true;
+    }
+}
