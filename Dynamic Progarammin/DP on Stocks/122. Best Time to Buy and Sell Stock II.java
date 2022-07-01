23bb78e9560a51ad8ace3d3f0f6e5b012ddf0c31
Rahul Maurya <93531189+rahul123gautam@users.noreply.github.com>
2022-07-01 19:15:27 +0530

    Create 122. Best Time to Buy and Sell Stock II.java

---
+++ Dynamic Progarammin/DP on Stocks/122. Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,86 @@
+Problem Link:- https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+
+
+
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+Example 2:
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Total profit is 4.
+Example 3:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 3 * 104
+0 <= prices[i] <= 104
+  
+  
+  
+  
+                                        ************************CODE************************
+                                        
+  
+  // Tabulation
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] > prices[i-1]){
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
+}
+
+
+// Recursion + Meomoization
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[][]dp = new int[prices.length][2];
+        for(int i = 0; i < dp.length; i++){
+            Arrays.fill(dp[i] , -1);
+        }
+        return profit(0 , 1 , prices , dp);
+    }
+    public int profit(int ind , int buy , int[]prices, int[][]dp){
+        if(ind == prices.length) return 0;
+        if(dp[ind][buy] != -1) return dp[ind][buy]; 
+        int profi = 0;
+        if(buy == 1){  // we can buy
+            int take = -prices[ind] + profit(ind+1 , 0 , prices, dp);
+            int not_take = 0 + profit(ind+1 , 1 , prices, dp);
+            profi = Math.max(take , not_take);
+        }else{
+            int sell = prices[ind] + profit(ind+1 , 1 , prices, dp);
+            int not_sell = profit(ind+1 , 0 , prices, dp);
+            profi = Math.max(sell , not_sell);
+        }
+        return dp[ind][buy] = profi;
+    }
+}
